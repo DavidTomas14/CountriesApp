9888b80dc6a871c00f8a27e2b3c4bc0c44c7a4ff
David Tomas <dtalonsocalderon@gmail.com>
2021-02-08 11:15:56 +0100

    MVVM acabado

--- app/src/main/java/com/example/countriesapp/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/countriesapp/ui/detail/DetailActivity.kt
@@ -51,72 +51,36 @@ class DetailActivity : AppCompatActivity() {
 
 
     }
-    private fun updateUi(model: DetailViewModel.UiModel){ with(binding) {
-        val icon = if(model.country.favourite) R.drawable.me_gusta_relleno else R.drawable.me_gusta
-        fab.setImageDrawable(ContextCompat.getDrawable(this@DetailActivity, icon))
+    private fun updateUi(model: DetailViewModel.UiModel) {
+        with(binding) {
+            val icon =
+                if (model.country.favourite) R.drawable.me_gusta_relleno else R.drawable.me_gusta
+            fab.setImageDrawable(ContextCompat.getDrawable(this@DetailActivity, icon))
 
-        countryDetailInfo.setCountryInfo(model.country)
+            countryDetailInfo.setCountryInfo(model.country)
 
-        with(model.country){
-            if(this != null){
-                binding.bandera.loadUrl(this.flagPath)
-                binding.toolbar.title = this.name
-            }
-
-
-
-
-
-        toolbar.setNavigationOnClickListener {
-            Toast.makeText(this@DetailActivity, "Navigation", Toast.LENGTH_SHORT).show()
-        }
-        toolbar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.favorite -> {
-                    Toast.makeText(this@DetailActivity, "Favorite", Toast.LENGTH_SHORT).show()
-                    true
+            with(model.country) {
+                if (this != null) {
+                    binding.bandera.loadUrl(this.flagPath)
+                    binding.toolbar.title = this.name
                 }
-                R.id.search -> {
-                    Toast.makeText(this@DetailActivity, "Search", Toast.LENGTH_SHORT).show()
-                    true
+
+                toolbar.setNavigationOnClickListener {
+                    onBackPressed()
                 }
-                R.id.more -> {
-                    Toast.makeText(this@DetailActivity, "More", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> false
+
+                textaco.text =
+                    "No dejes que nadie te diga que no puedes hacer algo. Ni siquiera yo. ¿Lo entiendes?" +
+                            " Si tienes un sueño, tienes que protegerlo. Cuando no pueden hacer algo, a las personas les gusta decirles " +
+                            "a los demás que tampoco podrán hacerlo. Si quieres algo, ve y búscalo. Punto.\n" +
+                            "Si quieres conquistar corazones y mentes, debes liderar con tu corazón y con tu cabeza. No creo que sea posible" +
+                            " tener una personalidad profesional de lunes a viernes y una personalidad real el resto del tiempo. Todo es profesional" +
+                            " y todo es personal al mismo tiempo.\n Hay una discordancia entre lo que la ciencia dice y lo que las empresas hacen. Y lo" +
+                            " que me preocupa, mientras estamos junto a los escombros del colapso de la economía, es que muchas organizaciones están tomando " +
+                            "sus decisiones y estableciendo sus políticas acerca del talento y el personal en función de presunciones obsoletas sin analizar y fundamentadas" +
+                            " más en el folclore que en la ciencia."
             }
         }
-
-        val customView = findViewById<CustomView>(R.id.customView)
-        customView.setView(
-            "https://images-na.ssl-images-amazon.com/images/I/616p3JxvgjL._AC_SX425_.jpg",
-            "Españita"
-        )
-
-
-
-        textaco.text =
-            "No dejes que nadie te diga que no puedes hacer algo. Ni siquiera yo. ¿Lo entiendes?" +
-                    " Si tienes un sueño, tienes que protegerlo. Cuando no pueden hacer algo, a las personas les gusta decirles " +
-                    "a los demás que tampoco podrán hacerlo. Si quieres algo, ve y búscalo. Punto.\n" +
-                    "Si quieres conquistar corazones y mentes, debes liderar con tu corazón y con tu cabeza. No creo que sea posible" +
-                    " tener una personalidad profesional de lunes a viernes y una personalidad real el resto del tiempo. Todo es profesional" +
-                    " y todo es personal al mismo tiempo.\n Hay una discordancia entre lo que la ciencia dice y lo que las empresas hacen. Y lo" +
-                    " que me preocupa, mientras estamos junto a los escombros del colapso de la economía, es que muchas organizaciones están tomando " +
-                    "sus decisiones y estableciendo sus políticas acerca del talento y el personal en función de presunciones obsoletas sin analizar y fundamentadas" +
-                    " más en el folclore que en la ciencia."
-
     }
-
-
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.top_app_bar, menu)
-        return true
-    }
-
 
 }
